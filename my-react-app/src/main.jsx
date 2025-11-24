5cce084b5f0aab58b1dfd9429cdb85fb0c290fed
Spandan2106 <sd672483@gmail.com>
2025-11-24 15:11:17 +0530

    comit

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import './index.css' // Global styles first
+import './App.css'   // Component-specific styles second
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
